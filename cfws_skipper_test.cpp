3c6011887297aa52dea0e0d9b0fd9fa3978da017
Richard <legalize@xmission.com>
2014-11-16 19:00:55 -0700

    Refactor: Extract suite for skipper tests

--- cfws_skipper_test.cpp
+++ cfws_skipper_test.cpp
@@ -11,102 +11,114 @@ using namespace boost::spirit::qi;
 namespace
 {
 
-bool validate(std::string const& skipped)
+struct fixture
 {
-    const std::string text{skipped + "STOP"};
-    auto start = text.begin();
+    fixture()
+        : suffix("STOP")
+    {}
+    bool execute(std::string const& skipped);
 
-    bool result = phrase_parse(start, text.end(),
-        eps, cfws::skipper<std::string::const_iterator>());
+    const std::string suffix;
+    std::string text;
+    std::string::iterator start;
+};
 
-    return result
-        && "STOP" == std::string{start, text.end()};
-}
-
-void execute(std::string const& skipped)
+bool fixture::execute(std::string const& skipped)
 {
-    BOOST_REQUIRE(validate(skipped));
+    text = skipped + suffix;
+    start = text.begin();
+
+    return phrase_parse(start, text.end(), eps,
+        cfws::skipper<std::string::const_iterator>());
 }
 
 }
+
+BOOST_FIXTURE_TEST_SUITE(cfws_skipper_suite, ::fixture);
+
+#define REQUIRE_SKIPPED(text_) \
+    BOOST_REQUIRE(execute(text_)); \
+    BOOST_REQUIRE_EQUAL(suffix, (std::string{start, text.end()}))
 
 BOOST_AUTO_TEST_CASE(skips_spaces)
 {
-    execute("  ");
+    REQUIRE_SKIPPED("  ");
 }
 
 BOOST_AUTO_TEST_CASE(skips_tabs)
 {
-    execute(" \t \t ");
+    REQUIRE_SKIPPED(" \t \t ");
 }
 
 BOOST_AUTO_TEST_CASE(skips_folded_whitespace_at_beginning)
 {
-    execute("\r\n ");
+    REQUIRE_SKIPPED("\r\n ");
 }
 
 BOOST_AUTO_TEST_CASE(skips_folded_whitespace_in_middle)
 {
-    execute("    \r\n \t\t");
+    REQUIRE_SKIPPED("    \r\n \t\t");
 }
 
 BOOST_AUTO_TEST_CASE(skips_folded_whitespace_at_end)
 {
-    execute(" \t \t \r\n ");
+    REQUIRE_SKIPPED(" \t \t \r\n ");
 }
 
 BOOST_AUTO_TEST_CASE(skips_comments)
 {
-    execute("(this is a comment)");
+    REQUIRE_SKIPPED("(this is a comment)");
 }
 
 BOOST_AUTO_TEST_CASE(skips_comments_with_leading_and_trailing_spaces)
 {
-    execute("( this is a comment )");
+    REQUIRE_SKIPPED("( this is a comment )");
 }
 
 BOOST_AUTO_TEST_CASE(quoted_whitespace)
 {
-    execute("(\\ \\\t)");
+    REQUIRE_SKIPPED("(\\ \\\t)");
 }
 
 BOOST_AUTO_TEST_CASE(quoted_visible)
 {
-    execute(R"text((\(\)\\))text");
+    REQUIRE_SKIPPED(R"text((\(\)\\))text");
 }
 
 BOOST_AUTO_TEST_CASE(nested_comments)
 {
-    execute("(this is a comment (with another comment inside))");
+    REQUIRE_SKIPPED("(this is a comment (with another comment inside))");
 }
 
 BOOST_AUTO_TEST_CASE(obsolete_comment_can_contain_control_characters)
 {
-    BOOST_REQUIRE(validate("(\x01\x02\x03\x04)"));
-    BOOST_REQUIRE(validate("(\x05\x06\x07\x08)"));
-    BOOST_REQUIRE(validate("(\x0b\x0c)"));
-    BOOST_REQUIRE(validate("(\x0e\x0f)"));
-    BOOST_REQUIRE(validate("(\x10\x11\x12\x13)"));
-    BOOST_REQUIRE(validate("(\x14\x15\x16\x17)"));
-    BOOST_REQUIRE(validate("(\x18\x19\x1a\x1b)"));
-    BOOST_REQUIRE(validate("(\x1c\x1d\x1e\x1f)"));
+    REQUIRE_SKIPPED("(\x01\x02\x03\x04)");
+    REQUIRE_SKIPPED("(\x05\x06\x07\x08)");
+    REQUIRE_SKIPPED("(\x0b\x0c)");
+    REQUIRE_SKIPPED("(\x0e\x0f)");
+    REQUIRE_SKIPPED("(\x10\x11\x12\x13)");
+    REQUIRE_SKIPPED("(\x14\x15\x16\x17)");
+    REQUIRE_SKIPPED("(\x18\x19\x1a\x1b)");
+    REQUIRE_SKIPPED("(\x1c\x1d\x1e\x1f)");
 }
 
 BOOST_AUTO_TEST_CASE(obsolete_quoted_null)
 {
     std::string null{};
     null.push_back(0);
-    execute(R"((\)" + null + ")");
+    REQUIRE_SKIPPED(R"((\)" + null + ")");
 }
 
 BOOST_AUTO_TEST_CASE(obsolete_quoted_control)
 {
-    BOOST_REQUIRE(validate("(\\\x01\\\x02\\\x03\\\x04)"));
-    BOOST_REQUIRE(validate("(\\\x05\\\x06\\\x07\\\x08)"));
-    BOOST_REQUIRE(validate("(\\\x0b\\\x0c)"));
-    BOOST_REQUIRE(validate("(\\\x0e\\\x0f)"));
-    BOOST_REQUIRE(validate("(\\\x10\\\x11\\\x12\\\x13)"));
-    BOOST_REQUIRE(validate("(\\\x14\\\x15\\\x16\\\x17)"));
-    BOOST_REQUIRE(validate("(\\\x18\\\x19\\\x1a\\\x1b)"));
-    BOOST_REQUIRE(validate("(\\\x1c\\\x1d\\\x1e\\\x1f)"));
+    REQUIRE_SKIPPED("(\\\x01\\\x02\\\x03\\\x04)");
+    REQUIRE_SKIPPED("(\\\x05\\\x06\\\x07\\\x08)");
+    REQUIRE_SKIPPED("(\\\x0b\\\x0c)");
+    REQUIRE_SKIPPED("(\\\x0e\\\x0f)");
+    REQUIRE_SKIPPED("(\\\x10\\\x11\\\x12\\\x13)");
+    REQUIRE_SKIPPED("(\\\x14\\\x15\\\x16\\\x17)");
+    REQUIRE_SKIPPED("(\\\x18\\\x19\\\x1a\\\x1b)");
+    REQUIRE_SKIPPED("(\\\x1c\\\x1d\\\x1e\\\x1f)");
 }
+
+BOOST_AUTO_TEST_SUITE_END();
